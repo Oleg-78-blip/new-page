d47a8be769bb8f5409c760d831f42934dd441730
Oleg Gor <goorobets78@gmail.com>
2024-03-14 17:58:09 +0200

    wow

---
+++ script.js/main.js
@@ -0,0 +1,5 @@
+console.log('helo world');
+let helo = document.querySelector('.nz');
+helo.addEventListener('click', function() {
+    helo.innerHTML = 'Південний острів — один з островів на яких розташована Нова Зеландія';
+})
